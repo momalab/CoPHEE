b137adcf83b5765e47474d7971aa6f42131fb3af
findnabeel <findnabeel@gmail.com>
2019-02-25 22:26:02 +0400

    Module directory contain only verif module. Deleting it, top copy both verif and rtl directories.

--- modules/ccs0101/verif/runtime.tcl
+++
@@ -1,2 +0,0 @@
-dump -add { ccs0101_tb }  -depth 0 -scope "." -filter=variable,generic,constant,pack_vh,parameter,define_cell,y_cell_lib,specify,child_cell
-run 100 ms
--- modules/crypto_lib/verif/runtime_montgomery_mul.tcl
+++
@@ -1,2 +0,0 @@
-dump -add { montgomery_mul_tb }  -depth 0 -scope "." -filter=variable,generic,constant,pack_vh,parameter,define_cell,y_cell_lib,specify,child_cell
-run 1 ms
