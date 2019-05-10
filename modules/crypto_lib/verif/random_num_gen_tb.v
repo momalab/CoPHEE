46e672fdd1ec821e879c380e4237b2c53c74bc49
findnabeel <findnabeel@gmail.com>
2019-05-10 23:40:27 +0400

    Added license header file

--- modules/crypto_lib/verif/random_num_gen_tb.v
+++ modules/crypto_lib/verif/random_num_gen_tb.v
@@ -1,3 +1,29 @@
+/**********************************************************************************
+    This module is part of the project CoPHEE.
+    CoPHEE: A co-processor for partially homomorphic encrypted encryption
+    Copyright (C) 2019  Michail Maniatakos
+    New York University Abu Dhabi, wp.nyu.edu/momalab/
+
+    If find any of our work useful, please cite our publication:
+      M. Nabeel, M. Ashraf, E. Chielle, N.G. Tsoutsos, and M. Maniatakos.
+      "CoPHEE: Co-processor for Partially Homomorphic Encrypted Execution". 
+      In: IEEE Hardware-Oriented Security and Trust (HOST). 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************************/
+
+
 `timescale 1 ns/1 ps
 
 module random_num_gen_tb (
